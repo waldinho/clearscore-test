b0452d4f109290551f8c8816e1128a170ef1ecd3
waldinho <james.waller1984@gmail.com>
2019-12-01 13:22:36 +1100

    Idea edit stuff hooked up to redux.

---
+++ build/precache-manifest.006be71e608662c261d0bdb69adb5795.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1611da424743e86e466216d8a2713ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d9c74a78a74cef6a21a3",
+    "url": "/static/css/main.645f80e3.chunk.css"
+  },
+  {
+    "revision": "fe40f97b52384ae7a88b",
+    "url": "/static/js/2.c4507c70.chunk.js"
+  },
+  {
+    "revision": "d9c74a78a74cef6a21a3",
+    "url": "/static/js/main.c14a26db.chunk.js"
+  },
+  {
+    "revision": "ab885c0bac6eb9b50f37",
+    "url": "/static/js/runtime-main.d5097448.js"
+  }
+]);
