eadcf12a6f85b058361262d592bdd8bfd05317d8
waldinho <james.waller1984@gmail.com>
2019-12-01 17:04:05 +1100

    character limit changed to 140

---
+++ build/precache-manifest.e3f4994c7036873e3b5f42851b4943a5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65f8b14e4390d38f9cbe13583590c92c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "918c01be8acac41700f9",
+    "url": "/static/css/main.83cae236.chunk.css"
+  },
+  {
+    "revision": "a1e6bdc2a8984be7ea67",
+    "url": "/static/js/2.5c06ab1b.chunk.js"
+  },
+  {
+    "revision": "918c01be8acac41700f9",
+    "url": "/static/js/main.1a1b1e56.chunk.js"
+  },
+  {
+    "revision": "ab885c0bac6eb9b50f37",
+    "url": "/static/js/runtime-main.d5097448.js"
+  }
+]);
