4f18576a009935ca20c21431330f44c8d927d5e1
waldinho <james.waller1984@gmail.com>
2019-12-01 14:35:42 +1100

    reducer correction, mapping to idea now.

---
+++ build/precache-manifest.825e052131fada5596c5e2c5867abea5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e41b0105168c06b037fc6810f6d11438",
+    "url": "/index.html"
+  },
+  {
+    "revision": "187d97619013506afdca",
+    "url": "/static/css/main.645f80e3.chunk.css"
+  },
+  {
+    "revision": "fe40f97b52384ae7a88b",
+    "url": "/static/js/2.c4507c70.chunk.js"
+  },
+  {
+    "revision": "187d97619013506afdca",
+    "url": "/static/js/main.0f691d63.chunk.js"
+  },
+  {
+    "revision": "ab885c0bac6eb9b50f37",
+    "url": "/static/js/runtime-main.d5097448.js"
+  }
+]);
